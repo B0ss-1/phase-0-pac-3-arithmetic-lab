886b471879eae4ed16a2d6c9a5fc7430105a3897
Gustavo Taveras <61476293+B0ss-1@users.noreply.github.com>
2021-11-28 09:24:17 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,62 @@
+ function add(string) {
+
+ }
+ function subtract(string) {
+     
+} 
+function multiply(string) {
+     
+} 
+function divide(string) {
+     
+}
+
+function add(a, b) {
+    return (a + b)  
+
+}
+
+function subtract(a, b) {
+    return (a - b)  
+
+}
+
+function multiply(a, b) {
+    return (a * b)  
+
+}
+
+function divide(a, b) {
+    return (a / b)  
+
+}
+
+function increment(a){
+    return (a + 1)
+}
+
+function decrement(a){
+    return (a - 1)
+}
+
+function makeInt( n ){
+    return (parseFloat(n)); 
+
+}
+
+function preserveDecimal(n ){
+    return (parseFloat(n )) 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
